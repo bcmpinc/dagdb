8baecd452bc894780fb5a6442f0145a2a057af2d
bcmpinc <bcmpinc@users.sourceforge.net>
2012-02-28 02:55:55 +0100

    Some bugfixes

--- src/base2.c
+++ src/base2.c
@@ -15,11 +15,15 @@ static void * file;
  */
 int dagdb_load(const char* database)
 {
-	int fd = open(database, O_RDWR | O_CREAT, 0x644);
+	int fd = open(database, O_RDWR | O_CREAT, 0644);
 	if (fd == -1) goto error;
 	file = mmap(NULL, 1<<24, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	ftruncate(fd,1024); // TODO: Remove here, use for growing file size.
 	if (file == MAP_FAILED) goto error;
+	printf("%p\n", file);
 	database_fd = fd;
+	((int*)file)[0] = 1; // TODO: Remove, 
+	printf("Opened DB\n");
 	return 0;
 
 error:
@@ -35,9 +39,14 @@ error:
  */
 void dagdb_unload()
 {
+	if (file) {
+		munmap(file, 1<<24);
+		file = 0;
+	}
 	if (database_fd) {
 		close(database_fd);
 		database_fd = 0;
+		printf("Closed DB\n");
 	}
 }
 
